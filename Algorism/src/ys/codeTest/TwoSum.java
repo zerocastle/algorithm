e688465acfbaa8df4e8a7969a3cfcbdee4b44780
kys <romg321@naver.com>
2020-02-07 00:39:22 +0900

    towsum

--- Algorism/src/ys/codeTest/TwoSum.java
+++ Algorism/src/ys/codeTest/TwoSum.java
@@ -1,5 +1,7 @@
 package ys.codeTest;
 
+import java.util.*;
+
 /*
  * TwoSum
  * example)
@@ -15,5 +17,42 @@ package ys.codeTest;
  */
 
 public class TwoSum {
+
+	public static void main(String[] args) {
+
+		TwoSum obj = new TwoSum();
+
+		int[] nums = { 11, 2 , 8, 21 };
+
+		int target = 10;
+
+		int[] result = obj.solve(nums, target);
+
+		System.out.println(Arrays.toString(result));
+		
+		System.out.println("test");
+
+	}
+
+	public int[] solve(int[] nums, int target) {
+		// TODO Auto-generated method stub
+		// 담을 그릇 선언을 해줘야줘
+		Map<Integer, Integer> map = new HashMap<>();
+		int[] array = new int[2];
+
+		for (int i = 0; i < nums.length; i++) {
+
+			if (map.containsKey(nums[i])) {
+				array[0] = map.get(nums[i]) + 1;
+				array[1] = i + 1;
+			} else {
+				map.put(target - nums[i], i);
+			}
+
+		}
+
+		return array;
+
+	}
 
 }
