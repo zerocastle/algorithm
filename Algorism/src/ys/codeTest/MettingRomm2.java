f2f7381a70e2381bcfc9a39ac0e28e43a0ff03a5
kys <romg321@naver.com>
2020-02-14 04:58:14 +0900

    문제풀고 커밋

---
+++ Algorism/src/ys/codeTest/MettingRomm2.java
@@ -0,0 +1,12 @@
+package ys.codeTest;
+
+/*
+ * problemn) 서로 겹치는 시간데를 비교 겹치는 갯수 즉 예약 방에 갯수를 구하는 문제
+ * 
+ * solution) 1. 소팅 -> 2.PriorityQueue 를 이용 (end time을 비교하여 우선순위를 스왑한다 -> 3. End <= start 합치기
+ * 
+ */
+
+public class MettingRomm2 {
+
+}
